6a3cab607f0bda6c70a6d4c2e378d3a2b0ed714f
Daek-You <choieorb1998@naver.com>
2023-06-05 19:51:05 +0900

    [Programmers] Lv2. 마법의 엘리베이터

---
+++ Programmers/Lv.2/마법의 엘리베이터.cpp
@@ -0,0 +1,37 @@
+#include <string>
+using namespace std;
+const int HALF_NUM = 5;
+const int divider = 10;
+
+bool IsWillGoUp(int quotient, int remainder) {
+    if (remainder == 5) {
+        int nextRemainder = quotient % divider;
+        return nextRemainder >= HALF_NUM;
+    }
+    return remainder > HALF_NUM;
+}
+
+
+int solution(int storey) {
+    int magicStoneCount = 0;
+
+    while (true) {
+        int quotient = storey / divider;
+        int remainder = storey % divider;
+        bool isWillGoUp = IsWillGoUp(quotient, remainder);
+        
+        int direction = isWillGoUp ? 1 : -1;
+        int moveDistance = min(10 - remainder, remainder);
+
+        storey += (moveDistance * direction);
+        magicStoneCount += moveDistance;
+
+        int nextFloorQuotient = storey / divider;
+        if (nextFloorQuotient == 0)
+            break;
+
+        storey = nextFloorQuotient;
+    }
+
+    return magicStoneCount;
+}
