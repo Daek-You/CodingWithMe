629243fc96088ca62c5cc3d2d73960036770ced8
Daek-You <choieorb1998@naver.com>
2023-06-20 17:51:37 +0900

    [Programmers] Lv2. 타겟 넘버

---
+++ Programmers/Lv.2/타겟 넘버.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+using namespace std;
+
+void DFS(const vector<int>& numbers, const int& target, int sum, int idx, int& answer) {
+    if (idx == numbers.size()) {
+        if (sum == target)
+            answer++;
+        return;
+    }
+
+    DFS(numbers, target, sum + numbers[idx], idx + 1, answer);
+    DFS(numbers, target, sum - numbers[idx], idx + 1, answer);
+}
+
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+    DFS(numbers, target, 0, 0, answer);
+    return answer;
+}
