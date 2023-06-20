d098d12d36b79f99fa30ccf77b026498e8ed933f
Daek-You <choieorb1998@naver.com>
2023-06-20 19:30:13 +0900

    [Programmers] Lv2. 전화번호 목록

---
+++ Programmers/Lv.2/전화번로 목록.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool IsPrefix(const string& shortStr, const string& longStr) {
+    return shortStr == longStr.substr(0, shortStr.size());
+}
+
+bool solution(vector<string> phone_book) {
+    if (phone_book.size() == 1)
+        return true;
+    
+    sort(phone_book.begin(), phone_book.end());
+    
+    for (int i = 0; i < phone_book.size() - 1; i++) {
+        if (IsPrefix(phone_book[i], phone_book[i+1]))
+            return false;
+    }
+
+    return true;
+}
