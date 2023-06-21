26f331b56dfd9e2e66961d58d5c39aba5ee91a5b
Daek-You <choieorb1998@naver.com>
2023-06-21 22:24:04 +0900

    [Programmers] Lv2. 오픈채팅방(수정)

--- Programmers/Lv.2/오픈채팅방.cpp
+++ Programmers/Lv.2/오픈채팅방.cpp
@@ -7,15 +7,15 @@ const int INSTRUCTION = 0;
 const int ID = 1;
 const int NICKNAME = 2;
 
-unordered_map<string, string> ChatMessages { {"Enter", "님이 들어왔습니다."}, {"Leave", "님이 나갔습니다."} };
+unordered_map<string, string> ChatMessages { {"Enter", "님이 들어왔습니다."}, { "Leave", "님이 나갔습니다." } };
 
 class User {
 public:
     User() { }
     User(string id, string nickName) : _id(id), _nickName(nickName) { }
-    string GetMessage(const string& instruction)   { return _nickName + ChatMessages[instruction]; }
+    string GetMessage(const string& instruction) { return _nickName + ChatMessages[instruction]; }
     void ChangeNickName(const string& newNickName) { _nickName = newNickName; }
-    string GetId()       { return _id; }
+    string GetId() { return _id; }
     string GetNickName() { return _nickName; }
 
 private:
@@ -43,38 +43,27 @@ vector<string> solution(vector<string> record) {
     for (const auto log : record) {
         vector<string> contents = Split(log);
         auto it = users.find(contents[ID]);
-        
+
         if (contents[INSTRUCTION] == "Leave")
             continue;
 
-        if (it == users.end()) {                    // 첫 접속
+        // 첫 접속
+        if (it == users.end()) {
             users.emplace(contents[ID], User(contents[ID], contents[NICKNAME]));
             continue;
         }
-        
-        if (contents[INSTRUCTION] == "Enter") {     // 나갔다 들어온 경우
-            users[contents[ID]].ChangeNickName(contents[NICKNAME]);
-            continue;
-        }
 
-        if (contents[INSTRUCTION] == "Change") {    // 닉네임 변경
-            users.find(contents[ID])->second.ChangeNickName(contents[NICKNAME]);
-        }
+        // 나갔다 들어왔거나, 닉네임을 변경하는 경우
+        if (contents[INSTRUCTION] == "Enter" or contents[INSTRUCTION] == "Change")
+            users[contents[ID]].ChangeNickName(contents[NICKNAME]);
     }
 
     // 출력 결과물 담기
     for (const auto log : record) {
         vector<string> informations = Split(log);
-        if(informations[INSTRUCTION] != "Change")
+        if (informations[INSTRUCTION] != "Change")
             answer.emplace_back(users[informations[ID]].GetMessage(informations[INSTRUCTION]));
     }
 
     return answer;
-}
-
-int main()
-{
-    vector<string> input{"Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"};
-    auto answer = solution(input);
-    return 0;
 }
