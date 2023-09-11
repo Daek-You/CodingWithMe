e3cc91ecfb2afafd5e0662629154b8d825b82b7e
Daek-You <choieorb1998@naver.com>
2023-09-11 15:28:06 +0900

    [Programmers] Lv.3 | 단어 변환

---
+++ Programmers/Lv.3/단어 변환.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+bool IsDifferenceOnlyOneCharacter(const string& source, const string& target)
+{
+    if (source.length() != target.length())
+        return false;
+
+    int differenceCount = 0;
+    int N = source.length();
+    
+    for (int i = 0; i < N; i++)
+    {
+        if (source[i] != target[i])
+            differenceCount++;
+
+        if (differenceCount > 1)
+            return false;
+    }
+
+    return true;
+}
+
+void DFS(string begin, const string& target, const vector<string>& words, vector<bool>& visited, int& answer, int step)
+{
+    if (answer <= step)
+        return;
+
+    if (begin == target)
+    {
+        answer = min(answer, step);
+        return;
+    }
+
+    for (int i = 0; i < words.size(); i++)
+    {
+        if (!IsDifferenceOnlyOneCharacter(words[i], begin) or visited[i])
+            continue;
+
+        visited[i] = true;
+        DFS(words[i], target, words, visited, answer, step + 1);
+        visited[i] = false;
+    }
+}
+
+int solution(string begin, string target, vector<string> words) {
+    int wordCount = words.size();
+    int answer = wordCount + 1;
+    vector<bool> visited(wordCount, false);
+
+    DFS(begin, target, words, visited, answer, 0);
+    return (answer == wordCount + 1) ? 0 : answer;
+}
